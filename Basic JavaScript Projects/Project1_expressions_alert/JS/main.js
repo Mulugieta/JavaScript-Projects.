200865e781eacf81f0745f3c6386a55be0edc001
Mulugieta <84230328+Mulugieta@users.noreply.github.com>
2025-05-21 18:36:24 -0400

    Basic JavaScript Projects Assignment
    Basic JavaScript Projects Assignments push

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var alertText = 'Welcome to' + ' my website'
+window.alert(alertText);
+var text2 = 'Javascript'
+var text = 'It\'s a beautiful day to learn ' +  text2 
+document.write(text); // this code prints a variable on the page
+
+document.write( 5 + 3); // this code adds the numbers
